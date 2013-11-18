3de81eb07585039ebfd8d25fcc3947946df2a714
Jon Goss <goss.jonathan@gmail.com>
2013-11-17 22:51:48 -0500

    Task 2: working netppm

--- netppm.rs
+++ netppm.rs
@@ -12,25 +12,20 @@ struct color
 
 fn main() 
 {
-	let width = 3;
-	let height = 2;
+	let width = 10;
+	let height = 10;
 	let mut colormap: ~[color] = ~[];
 	let mut x = 0;
 	let mut y = 0;
 
 	// Make image in mem as a float vector
-	// for y in range (0, height){
-	// 	for x in range (0, width){
-	// 		let tmp: color = color {red: (x / width), green: 0, blue: (y / height) };
-	// 		colormap.push(tmp);
-	// 	}
-	// }
 	while y < height {
 		while x < width {
-			let tmp: color = color {red: ((x / width) as float), green: (0 as float), blue: ((y / height) as float) };
+			let tmp: color = color {red: (x as float / width as float), green: (0 as float), blue: (y as float / height as float) };
 			colormap.push(tmp);
 			x += 1;
 		}
+		x = 0;
 		y += 1;
 	}
 
@@ -43,34 +38,21 @@ fn main()
 	print(" ");
 	println(fmt!("%?", height));
 	println("255");
-	// for y in range (0, height){
-	// 	for x in range(0, width){
-	// 		let red = (colormap[y*width+x].red * 255);
-	// 		let green = (colormap[y*width+x].green * 255);
-	// 		let blue = (colormap[y*width+x].blue * 255);
-	// 		print(fmt!("%i", red));
-	// 		print(" ");
-	// 		print(fmt!("%i", green));
-	// 		print(" ");
-	// 		print(fmt!("%i", blue));
-	// 		print(" ");
-	// 	}
-	// 	println("");
-	// }
 	while y < height {
 		while x < width {
 			let red = (colormap[y*width+x].red * (255 as float) );
 			let green = (colormap[y*width+x].green * (255 as float) );
 			let blue = (colormap[y*width+x].blue * (255 as float) );
-			print(fmt!("%?", red));
+			print(fmt!("%?", red as int));
 			print(" ");
-			print(fmt!("%?", green));
+			print(fmt!("%?", green as int));
 			print(" ");
-			print(fmt!("%?", blue));
+			print(fmt!("%?", blue as int));
 			print(" ");
 			x += 1;
 		}
 		println("");
+		x = 0;
 		y += 1;
 	}
 
